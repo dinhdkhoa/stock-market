200c703741de0071b7bc3ce81aa2d6c1ccbf6a22
dinhdkhoa <kevin_mccain1718@yahoo.com>
2025-02-26 10:46:37 +0700

    feat: POST, PUT,DELETE

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using stock_market.Data;
+using stock_market.Dtos.Stock;
 using stock_market.Mappers;
 
 namespace stock_market.Controllers;
@@ -30,6 +31,40 @@ public class StockController: ControllerBase
     {
         var stock = _context.Stocks.Find(id);
         if(stock == null) return NotFound();
+        return Ok(stock.ToStockDto());
+    }
+    [HttpPost]
+    public IActionResult Add(StockPostReqDto req)
+    {
+        var newStock = req.StockPostReqToStock();
+        _context.Stocks.Add(newStock);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(GetStockById), new {id = newStock.Id}, newStock.ToStockDto());
+    }
+    [HttpDelete("{id:int}")]
+    public IActionResult Delete([FromRoute] int id)
+    {
+        var stock = _context.Stocks.FirstOrDefault(x => x.Id == id);
+        if(stock == null) return NotFound();
+        _context.Stocks.Remove(stock);
+        _context.SaveChanges();
+        return NoContent();
+    }
+    [HttpPut("{id:int}")]
+    public IActionResult Update([FromRoute] int id, [FromBody] StockUpdateReqDto req)
+    
+    {
+        var stock = _context.Stocks.FirstOrDefault(x => x.Id == id);
+        if(stock == null) return NotFound();
+        // _context.Entry(req).CurrentValues.SetValues(stock);
+        stock.Symbol = req.Symbol;
+        stock.Industry = req.Industry;
+        stock.Purchase = req.Purchase;
+        stock.LastDiv = req.LastDiv;
+        stock.MarketCap = req.MarketCap;
+        stock.CompanyName = req.CompanyName;
+        
+        _context.SaveChanges();
         return Ok(stock.ToStockDto());
     }
 }
--- Mappers/StockMappers.cs
+++ Mappers/StockMappers.cs
@@ -17,5 +17,16 @@ public static class StockMappers
             LastDiv = stock.LastDiv,
             MarketCap = stock.MarketCap
         };
+    }public static Stock StockPostReqToStock(this StockPostReqDto req)
+    {
+        return new Stock
+        {
+            Symbol = req.Symbol,
+            Industry = req.Industry,
+            CompanyName = req.CompanyName,
+            Purchase = req.Purchase,
+            LastDiv = req.LastDiv,
+            MarketCap = req.MarketCap
+        };
     }
 }
