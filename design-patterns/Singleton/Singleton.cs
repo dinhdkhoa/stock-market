56036b2a1640677426b465801db126352c3d2b0c
dinhdkhoa <kevin_mccain1718@yahoo.com>
2025-03-04 16:15:10 +0700

    feat: Singleton

---
+++ design-patterns/Singleton/Singleton.cs
@@ -0,0 +1,23 @@
+﻿namespace design_patterns;
+
+public class Singleton
+{
+    private static Singleton? instance;
+
+    protected Singleton()
+    {
+        
+    }
+    public string Settings { get; private set; } = "Color Red";
+
+    public static Singleton Instance()
+    {
+        return instance ??= new Singleton();
+    }
+}
+
+
+// var object1 = Singleton.Instance();
+// var object2 = Singleton.Instance();
+//
+// if(object1 == object2) always true
